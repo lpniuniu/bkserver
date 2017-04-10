db6b8f8b75c9f859b4805e1c140ff936f4ffca25
fanfei <hzfanfei@corp.netease.com>
2017-04-10 21:50:47 +0800

    add get

--- server.js
+++ server.js
@@ -1,24 +1,72 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var query = require('connect-query')
+
+var db = mongoose.connect('mongodb://localhost/bkdb')
+var Schema = mongoose.Schema
+// 注册
+var Setting= new Schema({
+    _id:String,
+    key:String,
+    value:String
+})
+mongoose.model('Setting', Setting)
+
 var app = express();
 
-function logger(req, res, next) {
-    console.log('%s', req.method)
-    next()
-}
-app.use(logger)
-app.use(bodyParser.json())
 
-
-// 创建 application/json 解析
 var jsonParser = bodyParser.json()
-
-// POST /api/users 获取 JSON 编码的请求体
-app.post('/', jsonParser, function (req, res, err) {
-    if (!req.body) return res.sendStatus(400)
-    // create user in req.body
+app.post('/', jsonParser, function (req, res, next) {
+    if (req.body.length == 0) return res.sendStatus(400)
     console.log(req.body)
+    var setting = req.body['setting']
+    if (setting != undefined) {
+        var userId = setting['userId']
+        var key = setting['key']
+        var value = setting['value']
+
+        if (userId != undefined && userId == 'hzfanfei' && key != undefined  && value != undefined) {
+            console.log('数据正确')
+            var Setting = mongoose.model('Setting')
+            var setting = new Setting()
+            setting._id = userId
+            setting.key = key
+            setting.value = value
+            setting.save(function (err) {
+                if (!err) {
+                    console.log('save success !')
+                }
+            })
+            return res.sendStatus(200)
+        } else {
+            console.log('数据错误')
+            return res.sendStatus(500)
+        }
+    }
     res.end()
+})
+
+app.get('/', query(), function (req, res, next) {
+    console.log(req.query)
+    var userId = req.query['userId']
+    if (userId != undefined) {
+        var Setting = mongoose.model('Setting')
+        Setting.find({'_id': userId}, function (err, settings) {
+            if (err) {
+                res.sendStatus(500)
+                res.end('find')
+                return
+            }
+            if (settings.length > 0) {
+                res.jsonp(settings[0])
+            } else {
+                res.end()
+            }
+        })
+    } else {
+        res.sendStatus(500)
+    }
 })
 
 app.listen(3000)
